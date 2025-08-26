9c060eea490bc954480b05552ca137cf102ad1dd
pabloedu01 <pablo@pabloedu.com>
2025-08-26 15:14:06 +0000

    Fix Connector

--- flask_app/database.py
+++ flask_app/database.py
@@ -6,7 +6,11 @@ load_dotenv()
 
 def oracle():
     driver_class = "oracle.jdbc.OracleDriver"
-    jdbc_url = f"jdbc:oracle:thin:@{os.getenv('ORACLE_HOST')}:{os.getenv('ORACLE_PORT')}:{os.getenv('ORACLE_DATABASE')}"
+    jdbc_url = (
+        f"jdbc:oracle:thin:@{os.getenv('ORACLE_HOST')}:{os.getenv('ORACLE_PORT')}:{os.getenv('ORACLE_DATABASE')}"
+        f"?oracle.net.CONNECT_TIMEOUT=10000"  # 10 segundos de timeout
+        f"&oracle.net.READ_TIMEOUT=60000"     # 60 segundos de tempo de resposta
+    )
     driver_args = [
         "jdbc/oracle-jdbc-11.jar",
         "jdbc/postgresql-42.7.5.jar"
