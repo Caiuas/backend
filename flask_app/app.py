e209743e5f92716142692113f88efef663ec4136
pabloedu01 <pablo@pabloedu.com>
2025-08-26 15:14:19 +0000

    Adicionado novas roas

--- flask_app/app.py
+++ flask_app/app.py
@@ -8,13 +8,20 @@ from views.api.reports import reports_bp
 from views.api.site import site_bp
 from views.api.agendamento import agendamento_bp
 from views.api.dashboard import dashboard_bp
+from views.api.crm import crm_bp
+from views.api.veiculos_estoque import veiculos_estoque_bp
 load_dotenv()
 
 app = Flask(__name__)
+app.static_folder = 'static'
 cors = CORS(app)
+# static_files = /static
+
 # register app
 app.register_blueprint(clients_bp)
 app.register_blueprint(reports_bp)
 app.register_blueprint(site_bp)
 app.register_blueprint(agendamento_bp)
 app.register_blueprint(dashboard_bp)
+app.register_blueprint(crm_bp)
+app.register_blueprint(veiculos_estoque_bp)
