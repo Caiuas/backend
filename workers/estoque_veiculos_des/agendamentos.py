0579b82f58798e35e1808e8149dabd7ff42777d7
pabloedu01 <pablo@pabloedu.com>
2025-10-29 19:34:38 +0000

    Integracoes DES

--- workers/estoque_veiculos_des/agendamentos.py
+++ workers/estoque_veiculos_des/agendamentos.py
@@ -65,63 +65,83 @@ while True:
     conn, cur = conn_oracle()
 
     query = f"""
-        SELECT
-            em.cgc cnpjClient,
-            oa.cod_os_agenda numAppointment,
-            s.data_comeca dtAppointmentStart,
-            s.data_fim dtAppointmentEnd,
-            c.cod_cliente cpfCnpj,
-            c.nome name,
-            oa.chassi chassi,
-            oa.placa plate,
-            cf.ano AS ano_veiculo,         -- Coluna adicionada
-            cf.cor_veiculo AS cor_veiculo,  -- Coluna adicionada
-            pm.descricao_modelo,
-            oa.cod_empresa
-        FROM os_agenda_servicos s
-        LEFT JOIN CRM_EVENTOS ce ON 1=1
-            AND ce.COD_EMPRESA = s.crm_cod_empresa
-            AND ce.COD_EVENTO = s.CRM_COD_EVENTO
-        LEFT JOIN OS_AGENDA oa ON 1=1
-            AND oa.COD_EMPRESA = s.COD_EMPRESA
-            AND oa.COD_OS_AGENDA = s.COD_OS_AGENDA
-        LEFT JOIN CLIENTES c ON 1=1
-            AND c.COD_CLIENTE = oa.cod_cliente
-        LEFT JOIN PRISMA_BOX pb ON 1=1
-            AND pb.PRISMA = oa.PRISMA
-        LEFT JOIN produtos p ON 1=1
-            AND p.COD_PRODUTO = oa.COD_PRODUTO
-        LEFT JOIN PRODUTOS_MODELOS pm ON 1=1
-            AND pm.COD_PRODUTO = oa.COD_PRODUTO
-            AND pm.COD_MODELO = oa.COD_MODELO
-        LEFT JOIN EMPRESAS_USUARIOS eu ON 1=1
-            AND eu.nome = ce.RESPONSAVEL_PELO_EVENTO
-        LEFT JOIN os o ON 1=1
-            AND oa.COD_EMPRESA = o.COD_EMPRESA
-            AND oa.COD_OS_AGENDA = o.COD_OS_AGENDA
-            --AND o.complemento <> 'S'
-            AND o.ORCAMENTO <> 'S'
-        LEFT JOIN empresas_usuarios eu2 ON 1=1
-            AND eu2.NOME = oa.CONSULTOR
-        LEFT JOIN empresas_usuarios eu3 ON 1=1
-            AND eu3.NOME = oa.quem_abriu
-        LEFT JOIN empresas em ON 1=1
-            AND em.cod_empresa = oa.cod_empresa
-        -- >>> INÍCIO DA MODIFICAÇÃO <<<
-        -- Adiciona a junção com a tabela de frota para buscar ano e cor
-        LEFT JOIN clientes_frota cf ON 1=1
-            AND cf.chassi = oa.chassi
-            AND cf.vendido IS NOT NULL
-            AND cf.vendido <> 'S'
-        -- >>> FIM DA MODIFICAÇÃO <<<
-        WHERE 1=1
-            AND trunc(s.data_comeca) >= TO_DATE('{primeiro_dia}', 'YYYY-MM-DD')
-            AND trunc(s.data_fim) <= TO_DATE('{ultimo_dia}', 'YYYY-MM-DD')
+        SELECT *
+    FROM (
+        SELECT t.*, ROWNUM AS rn
+        FROM (
+            SELECT
+                em.cgc cnpjClient,
+                oa.cod_os_agenda numAppointment,
+                s.data_comeca dtAppointmentStart,
+                s.data_fim dtAppointmentEnd,
+                c.cod_cliente cpfCnpj,
+                CASE
+                    WHEN c.cod_cliente IS NULL THEN oa.cliente_nome
+                    ELSE c.nome
+                END name, 
+                oa.chassi chassi,
+                oa.placa plate,
+                cf.ano AS ano_veiculo,         -- Coluna adicionada
+                cf.cor_veiculo AS cor_veiculo,  -- Coluna adicionada
+                pm.descricao_modelo,
+                oa.cod_empresa
+            FROM os_agenda_servicos s
+            LEFT JOIN CRM_EVENTOS ce ON 1=1
+                AND ce.COD_EMPRESA = s.crm_cod_empresa
+                AND ce.COD_EVENTO = s.CRM_COD_EVENTO
+            LEFT JOIN OS_AGENDA oa ON 1=1
+                AND oa.COD_EMPRESA = s.COD_EMPRESA
+                AND oa.COD_OS_AGENDA = s.COD_OS_AGENDA
+            LEFT JOIN CLIENTES c ON 1=1
+                AND c.COD_CLIENTE = oa.cod_cliente
+            LEFT JOIN PRISMA_BOX pb ON 1=1
+                AND pb.PRISMA = oa.PRISMA
+            LEFT JOIN produtos p ON 1=1
+                AND p.COD_PRODUTO = oa.COD_PRODUTO
+            LEFT JOIN PRODUTOS_MODELOS pm ON 1=1
+                AND pm.COD_PRODUTO = oa.COD_PRODUTO
+                AND pm.COD_MODELO = oa.COD_MODELO
+            LEFT JOIN EMPRESAS_USUARIOS eu ON 1=1
+                AND eu.nome = ce.RESPONSAVEL_PELO_EVENTO
+            LEFT JOIN os o ON 1=1
+                AND oa.COD_EMPRESA = o.COD_EMPRESA
+                AND oa.COD_OS_AGENDA = o.COD_OS_AGENDA
+                AND o.ORCAMENTO <> 'S'
+            LEFT JOIN empresas_usuarios eu2 ON 1=1
+                AND eu2.NOME = oa.CONSULTOR
+            LEFT JOIN empresas_usuarios eu3 ON 1=1
+                AND eu3.NOME = oa.quem_abriu
+            LEFT JOIN empresas em ON 1=1
+                AND em.cod_empresa = oa.cod_empresa
+            LEFT JOIN clientes_frota cf ON 1=1
+                AND cf.chassi = oa.chassi
+                AND cf.vendido IS NOT NULL
+                AND cf.vendido <> 'S'
+            LEFT JOIN caiuas_os_agenda_des coad ON 1=1
+                AND coad.cod_empresa = oa.COD_EMPRESA 
+                AND coad.cod_os_agenda = oa.COD_OS_AGENDA 
+            WHERE 1=1
+                AND s.data_comeca IS NOT NULL
+                AND s.COD_EMPRESA IN (11,33)
+                --AND coad.data_envio IS null
+                AND trunc(s.data_comeca) >= trunc(CURRENT_DATE)
+            ORDER BY
+                s.data_comeca DESC
+            ) t 
+                )
+                WHERE 1=1
+                    --rn BETWEEN 1 AND 1
     """
     cur.execute(query)
     r = cur.fetchall()
     veiculos = []
-    print('Consultou o veículo')
+    if len(r) == 0:
+        print('Nenhum agendamento novo para enviar')
+        conn.close()
+        # espera 5 minutos
+        sleep(300)
+        continue
+    # print('Consultou o veículo')
     # print(r)
     for i in r:
 
@@ -174,10 +194,10 @@ while True:
         veiculos.append(veiculo)
 
     # Fecha a conexão
-    conn.close()
     
-    # with open("veiculos.json", "w") as f:
-    #     json.dump(veiculos, f)
+    
+    with open("veiculos.json", "w") as f:
+        json.dump(veiculos, f)
     # exit()
 
     url = "https://caiuas-miner-api.dealerequity.com.br/api/auth/login"
@@ -207,8 +227,22 @@ while True:
     }
     response = requests.request("POST", url, headers=headers, data=payload)
     print(response.status_code)
+    
     if response.status_code != 201:
+        try:
+            conn.close()
+        except:
+            pass
         raise Exception("Erro ao enviar veículos para a API")
+    # else:
+    #     for r in r:
+    #         query = f"""
+    #             insert into caiuas_os_agenda_des (cod_empresa, cod_os_agenda, data_envio) values
+    #             ({r[11]}, {r[1]}, SYSDATE)
+    #         """
+    #         cur.execute(query)
+    #         conn.commit()
+    #     conn.close()
     
     print(f"Apondamentos: {len(veiculos)}")
     # exit()
