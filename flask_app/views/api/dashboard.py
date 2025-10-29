62449a329f8f5a8768a7eba9499de8f65fca9a88
pabloedu01 <pablo@pabloedu.com>
2025-10-29 19:35:22 +0000

    Adicionado monitoramento para DES

--- flask_app/views/api/dashboard.py
+++ flask_app/views/api/dashboard.py
@@ -30,3 +30,68 @@ def get_totais():
         
     except Exception as e:
         return jsonify({'status': 'error', 'message': str(e)}), 500
+    
+@dashboard_bp.route('/api/dashboard/agendamentos_des', methods=['GET'])
+def get_agendamentos_des():
+    try:
+        conn_oracle, cur_oracle = oracle()
+        query = f"""
+        SELECT
+            count(*)
+        FROM os_agenda_servicos s
+        LEFT JOIN CRM_EVENTOS ce ON 1=1
+            AND ce.COD_EMPRESA = s.crm_cod_empresa
+            AND ce.COD_EVENTO = s.CRM_COD_EVENTO
+        LEFT JOIN OS_AGENDA oa ON 1=1
+            AND oa.COD_EMPRESA = s.COD_EMPRESA
+            AND oa.COD_OS_AGENDA = s.COD_OS_AGENDA
+        LEFT JOIN caiuas_os_agenda_des coad ON 1=1
+            AND coad.cod_empresa = oa.COD_EMPRESA 
+            AND coad.cod_os_agenda = oa.cod_os_agenda
+        WHERE 1=1
+            AND s.data_comeca IS NOT NULL
+            AND s.COD_EMPRESA IN (11,33)
+            AND coad.data_envio IS null
+        ORDER BY
+            s.data_comeca DESC
+        """
+        cur_oracle.execute(query)
+        result_oracle = cur_oracle.fetchall()
+        agendamentos_nao_enviados = result_oracle[0][0]
+        query = f"""
+        SELECT
+            count(*)
+        FROM os_agenda_servicos s
+        LEFT JOIN CRM_EVENTOS ce ON 1=1
+            AND ce.COD_EMPRESA = s.crm_cod_empresa
+            AND ce.COD_EVENTO = s.CRM_COD_EVENTO
+        LEFT JOIN OS_AGENDA oa ON 1=1
+            AND oa.COD_EMPRESA = s.COD_EMPRESA
+            AND oa.COD_OS_AGENDA = s.COD_OS_AGENDA
+        LEFT JOIN caiuas_os_agenda_des coad ON 1=1
+            AND coad.cod_empresa = oa.COD_EMPRESA 
+            AND coad.cod_os_agenda = oa.cod_os_agenda
+        WHERE 1=1
+            AND s.data_comeca IS NOT NULL
+            AND s.COD_EMPRESA IN (11,33)
+            AND coad.data_envio IS NOT NULL
+        ORDER BY
+            s.data_comeca DESC
+        """
+        cur_oracle.execute(query)
+        agendamentos_enviados = cur_oracle.fetchall()[0][0]
+        cur_oracle.close()
+        conn_oracle.close()
+        retorno = {}
+        retorno['agendamentos_nao_enviados'] = agendamentos_nao_enviados
+        retorno['agendamentos_enviados'] = agendamentos_enviados
+        retorno['total_agendamentos'] = agendamentos_nao_enviados + agendamentos_enviados
+        return jsonify(retorno), 200
+    except Exception as e:
+        try:
+            cur_oracle.close()
+            conn_oracle.close()
+        except:
+            pass
+        return jsonify({'status': 'error', 'message': str(e)}), 500
+    
