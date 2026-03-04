9306cb892a99cfdc2784dc5d4572b09b7c921c7f
pabloedu01 <pablo@pabloedu.com>
2026-03-04 23:02:58 +0000

    Adiiconado Postgresqite

--- flask_app/database.py
+++ flask_app/database.py
@@ -1,5 +1,7 @@
 import os
 import jaydebeapi
+import psycopg2
+import psycopg2.extras
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -31,3 +33,20 @@ def chatwoot():
     conn.jconn.setAutoCommit(False)
     cur = conn.cursor()
     return conn, cur
+
+def postgres_site():
+    try:
+        db_config = {
+            'dbname': os.environ.get('POSTGRES_SITE_DB'),
+            'user': os.environ.get('POSTGRES_SITE_USER'),
+            'password': os.environ.get('POSTGRES_SITE_PASSWORD'),
+            'host': os.environ.get('POSTGRES_SITE_HOST'),
+            'port': os.environ.get('POSTGRES_SITE_PORT')
+        }
+        conn = psycopg2.connect(**db_config, cursor_factory=psycopg2.extras.DictCursor)
+        cur = conn.cursor()
+        return conn, cur
+    except Exception as e:
+        print(f"Error connecting to Postgres: {e}")
+        raise
+    
